27d2c80cdfcb4aa3da30b0b0052d2f3cf31a87f6
Narbeh Shahnazarian <narbwork@gmail.com>
2022-12-11 13:38:04 -0800

    Adds filecoin fetch implementation

--- src/client/storedat/storedat.api.client.ts
+++ src/client/storedat/storedat.api.client.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 import { GLOSSETA_API_GRAPHQL_URL } from "../../utils/endpoints";
 import { GET_GLOSSARY_TERM } from "../../resources/graphql/queries/GetGlossaryTerm";
-import { GlossaryTerm } from "../../resources/graphql/types/storedat-gql-types";
+import { Content, GlossaryTerm } from "../../resources/graphql/types/storedat-gql-types";
+import { GET_DATA_FROM_FILECOIN } from "../../resources/graphql/queries/GetDataFromFilecoin";
 
 const glossetaClient = new ApolloClient({
     uri: GLOSSETA_API_GRAPHQL_URL,
@@ -31,4 +32,24 @@ export const getGlossaryTerm = async (term: string, locale: string): Promise<Glo
     }
 
     return glossaryTerm;
+}
+
+export const getDataFromFilecoin = async (cid: string): Promise<Content[]> => {
+    let content: Content[] = [];
+
+    try {
+        const { data } = await glossetaClient.query({
+            query: GET_DATA_FROM_FILECOIN,
+            variables: {
+                "cid": cid,
+            },
+        });
+
+        content = data.GetDataFromFilecoin;
+    } catch (error) {
+        console.log(`[Unable to find data from Filecoin] cid=${cid}, error=${error.message}`)
+        throw new Error(error)
+    }
+
+    return content;
 }
